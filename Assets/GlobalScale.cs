eaefdcef35fbeabb0b328f0bcca7d00ccb387f13
Finlay Braithwaite <3164558@student.ocadu.ca>
2018-02-02 23:14:34 -0500

    broken first versions of keys and global scale.

---
+++ Assets/GlobalScale.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalScale : MonoBehaviour {
+    public string keyName;
+    //public bool[] key = new bool[12];
+    private bool[] major = { true, false, true, false, true, true, false, true, false, true, false, true };
+    private bool[] minor = { true, false, true, true, false, true, false, true, true, false, true, false };
+
+    
+    
+    
+    
+    // Use this for initialization
+    void Awake () {
+        bool[] key = minor;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float jump = 2f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(0, jump, 0, ForceMode.Impulse);
+            Debug.Log("JUMP!");
+            //rb.AddForce(movement * speed);
+            //rb.transform.position.y = jumpY + 5;
+
+        }
+        else { rb.AddForce(movement * speed); }
+    }
+
+
+}
